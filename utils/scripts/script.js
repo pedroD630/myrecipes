f837848f5e6699793528e07f94e121bff0452834
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2024-02-04 16:02:21 -0300

    feat: Exibe menu ao clicar

---
+++ utils/scripts/script.js
@@ -0,0 +1,9 @@
+document.getElementById("open-menu").addEventListener("click", function () {
+    document.querySelector(".nav-menu").classList.toggle("visible");
+    document.querySelector("#open-menu").classList.toggle("visible");
+});
+
+document.querySelector("#close").addEventListener("click", function () {
+    document.querySelector(".nav-menu").classList.toggle("visible");
+    document.querySelector("#open-menu").classList.toggle("visible");
+})
