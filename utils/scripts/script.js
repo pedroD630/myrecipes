56bb51330c5df6c97d4035f7f54d885c5b511101
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2024-02-05 15:56:57 -0300

    Troca id do botão open-menu

--- utils/scripts/script.js
+++ utils/scripts/script.js
@@ -1,9 +1,9 @@
-document.getElementById("open-menu").addEventListener("click", function () {
+document.getElementById("mobile-open-menu").addEventListener("click", function () {
     document.querySelector(".nav-menu").classList.toggle("visible");
-    document.querySelector("#open-menu").classList.toggle("visible");
+    document.querySelector("#mobile-open-menu").classList.toggle("visible");
 });
 
 document.querySelector("#close").addEventListener("click", function () {
     document.querySelector(".nav-menu").classList.toggle("visible");
-    document.querySelector("#open-menu").classList.toggle("visible");
+    document.querySelector("#mobile-open-menu").classList.toggle("visible");
 })
