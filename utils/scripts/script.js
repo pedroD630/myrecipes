6df77f65d5a29345d327ec703862bd61ca29d7f9
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2024-02-08 14:47:51 -0300

    Iniciar script após carregamento da página

--- utils/scripts/script.js
+++ utils/scripts/script.js
@@ -1,9 +1,11 @@
-document.getElementById("mobile-open-menu").addEventListener("click", function () {
-    document.querySelector(".nav-menu").classList.toggle("visible");
-    document.querySelector("#mobile-open-menu").classList.toggle("visible");
+document.addEventListener('DOMContentLoaded',() => {
+    document.getElementById("mobile-open-menu").addEventListener("click", function () {
+        document.querySelector(".nav-menu").classList.toggle("visible");
+        document.querySelector("#mobile-open-menu").classList.toggle("visible");
+    });
+    
+    document.querySelector("#close").addEventListener("click", function () {
+        document.querySelector(".nav-menu").classList.toggle("visible");
+        document.querySelector("#mobile-open-menu").classList.toggle("visible");
+    })
 });
-
-document.querySelector("#close").addEventListener("click", function () {
-    document.querySelector(".nav-menu").classList.toggle("visible");
-    document.querySelector("#mobile-open-menu").classList.toggle("visible");
-})
