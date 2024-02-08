cf2fcb684f7842e66d7b7bbd7860513cc352630c
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2024-02-08 14:48:39 -0300

    style: Adicionar ponto e vírgula

--- utils/scripts/script.js
+++ utils/scripts/script.js
@@ -7,5 +7,5 @@ document.addEventListener('DOMContentLoaded',() => {
     document.querySelector("#close").addEventListener("click", function () {
         document.querySelector(".nav-menu").classList.toggle("visible");
         document.querySelector("#mobile-open-menu").classList.toggle("visible");
-    })
+    });
 });
